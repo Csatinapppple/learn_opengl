54ecfa01880af800f4791cadf4625c915e951202
cuds <ana@r.c>
2026-01-27 04:33:42 +0100

    Input

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include "../include/glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main() {
+	printf("fuck\n");
+	if (!glfwInit()) return -1;
+	
+	printf("fuck\n");
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "fuckass title", NULL, NULL);
+	
+	if (window == NULL){
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	
+	glViewport(0, 0, 800, 600);
+	
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while(!glfwWindowShouldClose(window)){
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
