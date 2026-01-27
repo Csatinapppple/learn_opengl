a1d2c661bba44d7d8904c426fb3ae75edccf63eb
cuds <ana@r.c>
2026-01-27 01:31:38 +0100

    fixed

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
+//#include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <stdio.h>
-#include "../include/glad/glad.h"
 
 
 int main() {
 	
+	if (!glfwInit()) return -1;
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
 	printf("Window memory fuck");
 	GLFWwindow* window = glfwCreateWindow(640, 480, "fuckass title", NULL, NULL);
