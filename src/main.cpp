7720e33e696c2ae588889a866614f4df81945279
cuds <ana@r.c>
2026-02-23 18:20:03 +0100

    exercise-1 done

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,15 @@ void processInput(GLFWwindow *window);
 void handleId(unsigned int shaderId, GLenum pname);
 
 float vertices[] = {
-	0.5f, 0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	-0.5f, -0.5f, 0.0f,
-	-0.5f, 0.5f, 0.0f
+	-0.75f, -0.5f, 0.0f,
+	-0.25f, -0.5f, 0.0f,
+	-0.5f, 0.5f, 0.0f,
+
+	0.75f, -0.5f, 0.0f,
+	0.25f, -0.5f, 0.0f,
+	0.5f, 0.5f, 0.0f
 };
 
-unsigned int indices[] = {
-	0, 1, 3,
-	1, 2, 3
-};
 
 int main() {
 	
@@ -75,15 +74,12 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -94,8 +90,6 @@ int main() {
 
 	glBindVertexArray(0);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
 	while(!glfwWindowShouldClose(window)){
 		processInput(window);
 		
@@ -104,7 +98,7 @@ int main() {
 		
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
