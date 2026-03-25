acb8a679f818cae8cb0bec630ed2c3707cfea988
Csatinapppple <cstinagarplz@hotmail.com>
2026-03-25 23:23:03 +0100

    reverse texture coordinates

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -8,8 +8,11 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main() {
+	
+	vec2 reverse_coord = vec2(texCoord.x * -1.0, texCoord.y);
+
 	vec4 first_texture = texture(texture1, texCoord);
-	vec4 second_texture = texture(texture2, texCoord);
+	vec4 second_texture = texture(texture2, reverse_coord);
 
 	FragColor = mix(first_texture, second_texture, 0.5);
 }
