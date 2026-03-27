768d72dd39c09bf3ece06d15fe92ab9f381d3ce9
Csatinapppple <cstinagarplz@hotmail.com>
2026-03-27 04:46:05 +0100

    added second box through transformation

--- src/main.cpp
+++ src/main.cpp
@@ -145,6 +145,16 @@ int main() {
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+		float sinTime = std::sin((float)glfwGetTime());
+
+		trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, glm::vec3(-0.5, 0.5, 0));
+		trans = glm::scale(trans, glm::vec3(sinTime, sinTime, sinTime));
+
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+		
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
