43d4423eac85cf8677ebb63feb55a27839ff46c2
Csatinapppple <cstinagarplz@hotmail.com>
2026-04-25 23:02:40 +0200

    Directional Lights

--- src/main.cpp
+++ src/main.cpp
@@ -66,6 +66,19 @@ float vertices[] = {
     -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
 };
 
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
@@ -169,10 +182,10 @@ int main() {
 		
 		shader.use();
 
-		shader.setVec3f("light.position", lightPos); 
 		shader.setVec3f("light.ambient", glm::vec3(0.2f, 0.2f, 0.2f)); 
 		shader.setVec3f("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
     shader.setVec3f("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+    shader.setVec3f("light.direction", glm::vec3(-0.2f, -1.0f, -0.3f));
 
 		shader.setVec3f("viewPos", camera.Position);
 
@@ -182,11 +195,15 @@ int main() {
 		shader.setMatrix4f("projection", projection);
 		shader.setMatrix4f("view", view);
 
-		glm::mat4 model = glm::mat4(1.0f);
-		shader.setMatrix4f("model", model);
-		
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++){
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0, 0.3f, 0.5f));
+			shader.setMatrix4f("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		lightShader.use();
 		
