2d187440a8a066a5277039375a70251a193fe979
Csatinapppple <cstinagarplz@hotmail.com>
2026-05-11 07:52:09 +0200

    Visualizing the depth buffer

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,6 +7,7 @@ out vec4 FragColor;
 uniform sampler2D texture1;
 
 void main() {
-	FragColor = texture(texture1, TexCoords);
+	//FragColor = texture(texture1, TexCoords);
+	FragColor = vec4(vec3(gl_FragCoord.z),1);
 }
 
