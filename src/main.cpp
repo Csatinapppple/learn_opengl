946923f1a793d47c3793344533f5527b4aaa55cc
Csatinapppple <cstinagarplz@hotmail.com>
2026-05-16 05:56:51 +0200

    Counter-Clockwise Ordering

--- src/main.cpp
+++ src/main.cpp
@@ -24,45 +24,45 @@ GLuint loadTexture(const char* path) ;
 float cubeVertices[] = {
 	// Back face
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
-	0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right    
 	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right              
+	0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right    
 	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left                
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
 	// Front face
 	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
-	0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
 	0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right        
 	0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
 	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left        
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
 	// Left face
 	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
 	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left       
 	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
 	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
 	// Right face
 	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
-	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right      
 	0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right          
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right      
 	0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
-	0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
 	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+	0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
 	// Bottom face          
 	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
-	0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
 	0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left        
 	0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
 	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
 	// Top face
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
-	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
 	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right                 
-	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
 	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f, // bottom-left  
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f  // top-left              
 };
 
@@ -70,11 +70,11 @@ float cubeVertices[] = {
 float planeVertices[] = {
 	// positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
 	5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+	-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
 	-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
-	-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
 
-	5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
 	-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+	5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
 	5.0f, -0.5f, -5.0f,  2.0f, 2.0f								
 };
 
@@ -156,7 +156,7 @@ int main() {
 
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
-	glFrontFace(GL_CW);
+	glFrontFace(GL_CCW);
 
 	while(!glfwWindowShouldClose(window)){
 		float currentFrame = glfwGetTime();
