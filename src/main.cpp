f609afc7d15137afee63545086ee3e8321b0ffa8
Csatinapppple <cstinagarplz@hotmail.com>
2026-05-17 23:01:41 +0200

    rear view mirror

--- src/main.cpp
+++ src/main.cpp
@@ -76,15 +76,15 @@ float planeVertices[] = {
 	5.0f, -0.5f, -5.0f,  2.0f, 2.0f								
 };
 
-float quadVertices[] = {  
-    // positions   // texCoords
-    -1.0f,  1.0f,  0.0f, 1.0f,
-    -1.0f, -1.0f,  0.0f, 0.0f,
-     1.0f, -1.0f,  1.0f, 0.0f,
+float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates. NOTE that this plane is now much smaller and at the top of the screen
+	// positions   // texCoords
+	-0.3f,  1.0f,  0.0f, 1.0f,
+	-0.3f,  0.7f,  0.0f, 0.0f,
+	0.3f,  0.7f,  1.0f, 0.0f,
 
-    -1.0f,  1.0f,  0.0f, 1.0f,
-     1.0f, -1.0f,  1.0f, 0.0f,
-     1.0f,  1.0f,  1.0f, 1.0f
+	-0.3f,  1.0f,  0.0f, 1.0f,
+	0.3f,  0.7f,  1.0f, 0.0f,
+	0.3f,  1.0f,  1.0f, 1.0f
 };
 
 const unsigned int SCR_WIDTH = 800;
@@ -207,30 +207,45 @@ int main() {
 	screenShader.setInt("screenTex", 0);
 
 	glDepthFunc(GL_LESS);
-
-	while(!glfwWindowShouldClose(window)){
-		float currentFrame = glfwGetTime();
+	while (!glfwWindowShouldClose(window))
+	{
+		// per-frame time logic
+		// --------------------
+		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
+
+		// input
+		// -----
 		processInput(window);
-	
+
+
+		// first render pass: mirror texture.
+		// bind to framebuffer and draw to color texture as we normally 
+		// would, but with the view camera reversed.
+		// bind to framebuffer and draw scene as we normally would to color texture 
+		// ------------------------------------------------------------------------
 		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+
+		// make sure we clear the framebuffer's content
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glEnable(GL_DEPTH_TEST);
 
 		shader.use();
 		glm::mat4 model = glm::mat4(1.0f);
+		camera.Yaw   += 180.0f; // rotate the camera's yaw 180 degrees around
+		camera.ProcessMouseMovement(0, 0, false); // call this to make sure it updates its camera vectors, note that we disable pitch constrains for this specific case (otherwise we can't reverse camera's pitch values)
 		glm::mat4 view = camera.GetViewMatrix();
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), 
-				(float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
+		camera.Yaw   -= 180.0f; // reset it back to its original orientation
+		camera.ProcessMouseMovement(0, 0, true); 
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		shader.setMatrix4f("view", view);
 		shader.setMatrix4f("projection", projection);
-
+		// cubes
 		glBindVertexArray(cubeVAO);
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, cubeTexture); 	
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
 		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
 		shader.setMatrix4f("model", model);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -245,21 +260,49 @@ int main() {
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
 
+		// second render pass: draw as normal
+		// ----------------------------------
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		model = glm::mat4(1.0f);
+		view = camera.GetViewMatrix();
+		shader.setMatrix4f("view", view);
+
+		// cubes
+		glBindVertexArray(cubeVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		shader.setMatrix4f("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		shader.setMatrix4f("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		// floor
+		glBindVertexArray(floorVAO);
+		glBindTexture(GL_TEXTURE_2D, floorTexture);
+		shader.setMatrix4f("model", glm::mat4(1.0f));
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+		// now draw the mirror quad with screen texture
+		// --------------------------------------------
+		glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
 
 		screenShader.use();
 		glBindVertexArray(screenVAO);
-		glDisable(GL_DEPTH_TEST);
-		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);	// use the color attachment texture as the texture of the quad plane
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
-
 	}
 
 	glfwTerminate();
