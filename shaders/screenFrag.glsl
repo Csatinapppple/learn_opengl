b6c124d765cf5eee7059c2759859c44181c78f86
Csatinapppple <cstinagarplz@hotmail.com>
2026-05-17 20:06:04 +0200

    Framebuffers grayscale

--- shaders/screenFrag.glsl
+++ shaders/screenFrag.glsl
@@ -5,6 +5,9 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTex;
 
-void main() {
-	FragColor = vec4(vec3(1.0 - texture(screenTex, TexCoords)), 1.0);
-}
+void main() 
+{
+	FragColor = texture(screenTex, TexCoords);
+	float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+	FragColor = vec4(average, average, average, 1.0);
+}   
