34041575866a4b7efc6e1e0cfd4ede49a224a707
Csatinapppple <cstinagarplz@hotmail.com>
2026-05-30 15:14:51 +0200

    gl_FrontFacing

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,7 +7,7 @@ out vec4 FragColor;
 uniform sampler2D texture1;
 
 void main() {
-	if (gl_FragCoord.x < 400) FragColor = vec4(1.0, 0,0,1.0);
-	else  FragColor = vec4(0, 1, 0,1);
+	if(gl_FrontFacing) FragColor = texture(texture1, TexCoords);
+	else FragColor = vec4(1,0,0,1);
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -22,48 +22,48 @@ void scroll_callback(GLFWwindow* window, double, double);
 GLuint loadTexture(const char* path) ;
 
 float cubeVertices[] = {
-	// positions          // texture Coords
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-	0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	// Back face
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right              
+	0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right    
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left                
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+	// Front face
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right        
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left        
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+	// Left face
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left       
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+	// Right face
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right          
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right      
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+	0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+	// Bottom face          
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+	0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left        
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+	// Top face
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right                 
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f, // bottom-left  
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f  // top-left              
 };
 
 const unsigned int SCR_WIDTH = 800;
