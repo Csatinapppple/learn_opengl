e2dfafdaa508bbc5085907274ac2a82f7dc76214
Csatinapppple <cstinagarplz@hotmail.com>
2026-06-14 08:18:13 +0200

    Instanced Arrays

--- src/main.cpp
+++ src/main.cpp
@@ -87,10 +87,9 @@ int main() {
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2*sizeof(float)));
-	glBindVertexArray(0);
 
 	shader.use();
-
+	
 	glDepthFunc(GL_LESS);
 
 	glm::vec2 translations[100];
@@ -107,10 +106,15 @@ int main() {
 		}
 	}
 	
-	for(unsigned int i = 0; i < 100; i++)
-	{
-			shader.setVec2f(("offsets[" + std::to_string(i) + "]"), translations[i]);
-	}  
+	GLuint instanceVBO;
+	glGenBuffers(1, &instanceVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribDivisor(2, 1);
+	glBindVertexArray(0);
 
 	while(!glfwWindowShouldClose(window)){
 		float currentFrame = glfwGetTime();
